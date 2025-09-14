4a2edb2170182a19473b592bc7702210450c8e8f
snoussi <snoussiabderrahman01@gmail.com>
2025-09-14 20:06:54 +0100

    Implement Zobrist hashing and transposition table for improved alpha-beta pruning in minimax algorithm

--- minimax/algorithm.py
+++ minimax/algorithm.py
@@ -1,4 +1,230 @@
+from copy import deepcopy
+import random
+from checkers.constants import BLACK, CREAM, ROWS, COLS
 
-# Python obfuscation by freecodingtools.org
-                    
-_ = lambda __ : __import__('zlib').decompress(__import__('base64').b64decode(__[::-1]));exec((_)(b'==Arej2Ef8/fffu/adrBxk7fepBu3B4JBNedeHXRSrZ8bs3koL1vqGLDPEMOUhQGWzJ3QLBJwRKEA9bFBBAdtZ4dsZ0ucYzlOsYfxuXE4ygdTgYnBHxVcEoq7mFxcIdbCgivoPvWMWuPMK+pr9IfrnaV1/gQkYZNE27dSwPkwcJimBYXPmi/2obk13utfgVlEaq7R4+WZJwxBlI5E0QEE+2esdjjqJkf3ejTIa9jQ8A38w7OP0x7DupobgfZ0f5p14z0dXxbj8JBjpTCtQD1EFHbzjMr182N2SLbIuKYaUeepnH71jLAAwHrUu07XVW8hUcUfyYAUSmqUOMTHNv+7XyGZunUthMNkW+IODyMyBIy3TYRtGBYqj4q6jyjAriXp83UjU2nu12kIFmSjEWXH43v9FRT/rsmNS9ztnhOGIfbtNYZLGSYR5y3v3BA633aje/AKNgvtkjCfEj6sskjWqGBMRGNYWzTrL9i94cxHgAIX7kXnxETwif2Cp5F3TvP1oGHkfXW4B8jr7kbqBC7WTxuxXQLzkAEOIdRNeC+jOS8UO6xZezqxN/y21yRwPNxvaCzVKyUSJWQQ069H308or1lddklFPAxYm3jAt0aFQ0U958fKmoYCxq9FmeZnbuV9ZcpTVyyhcJzKXpcJ4/Fn94Cc4vNnnwl3Lti6g6I2rPcUkZL3VObA+WwfYgM51m1Um6TfmM6N3ArrWS/E/MEvCYGTOODSaYXCk4nXJv9pISLCcqp0xQFGE2X/o6JTlZ7mu2ypU5d+ah4N/CCXIFvQsQla8I7NyRQ8dIAF9nfaPdlDvtvzZWISTtM6RBrbvmcdrP3SZbHs6mSFyDjpDaOpZ+HEtqU/Lilzcq/kjjuiC/oZW1Uk8gVMZl9tQ53yHD6WP6lVd2bDtpZyglEzexSKmsRfqpgiQEGvThooouGH9OEUjEfouXl6/hdgcwrI0rMBPOLulWm6HRNoP4qFBXByCiQBgBsSFpFpeyQOqz/1lrLcFHGqXmi0cxKnXEgM7xK4TZGJyuxTfxvxDMp6766Qz4lfDOS4fl+PZyZILBveS7nSXh/yqfibv+fv+Dn0+ues9CUp1/Zx2JVW4Ww91MU6G2y5UkQagaYTtvvfDahwb0CPgjaT8ZqfLsMqOxSSclaNDdz5PZsko5jDSa9R7m9zG7b+K+FSrHcZVbPGSgdF87jR4Nw+hDjJeCQ+TssFgmWyRCI4S1lGvgEo88hQcH+BBNttMZJw3uRTuTXJdj5tKrDx2eASuRwFUT/3+QBR8iaYVpVNBj16/MOSjUUZ9QWNGnP+qqDvF5lmrTmaTiXBG21bQOcOmR6QMwerX897yDDNYnIqWlJ2hAqslB8XO6hBYKLBRYpx38TUv43nOFYDjbb8+V5C/iz1vIUY8bhDqBKFdYSVqAtg772sm90KVAzhN9lEK2mmkEKHagwuf2yq+s8+X1T8Lq4BnIFS/TtCAaO3PvAgY0weI2Hi/9ggbvBKYZmJSvi6x8FxzEG/IbkQoGj3gHw81YwR6AovxzuYhTIpFWJBVgXP4TEHiYPsImiDcg7tgMGQwaOCyWq5a8nthQ+SkTVqL1aToIXWRKPulQbCZKFOW9qojiHPYaMoyK/A+B2ZyIuEUIT9NJd/xl4T/CQNVDRxbdSkP8EZh9Jn8Ljd7YATWmS9ajGnKxInnbNwz9ROaWY0lqS1d3T8G30A/QVgQDeS/nAO3TxCU+wYZxL6N18Rk49Jv9rBeNbgJyEcBayxx9VB3NGVF6n9+s6/bAH1lxMUDZyaQyhC6YFPJJa+fWJCUumC1CKKJpeX4C2m6zO/BOGXrkHo8XHTRHOYL/GmeyC5YKT8TGOxOqNjU2B7ypPyQCsd3mXSdI6jqvB+l9kcuz6ZSnGLdZ0r48tEgWUmbIgrUn5XjX9N7573lQgm4knLPHjy+6f0nxQHJZfZPx+qS7syarFLdNYbYFRiBCzDwUrM/feCjp2GJ64cbimQnDz/dgBt/aPIBytu2z4BEOaa+birDgS7C4rfLdymhKAOrUti0fxzfJTk3s/vs88FCkAnqycfmccSMgdrs+cbMnUVF6UuastqfpBFg2hUyGJLt+2hB2xfp0lIA+/9xFQsbiTpA+Oj/FwofpN7afuP4jappqbx/Pmp4wQO/eyYORaA6ha3NsLrvoMl/dTlnMGRwV9Lg0sPI76Jt9mYjH8iFDbBhl7q5WxwlH/r5o5rLwG5VxiuIVZtKmhU6mfAKgUwp5WzxPJHV1MXCqBjImceHa+RempoIpB0FmYSCmhWoGILqcKUS0dkDZ3Q7vU9uIpI3OZHvV6D6OqXOaSAn/Bvz9HmFSioaNKuLEjijFKB1NxbTC8P5sKX6p6RUv+kyDJo9X4QF4OgDCONioNK/qEVLbkxb5SO9BqfSMIKvegSu5VAGJNrEIj9vS6mu4kaYbJXD5MHvnuWmj3ovvHqVRwQ/hAEwkdvSATS9Qmej7JfQ5+PYje08mosA3Qs8JisO5u7uiNLEj1/YD44yyvsfQYTzPWpfrat6APGxqE0O2mgzsqQ6vJX6rp6viGei98upp7Qd0kxhiGJ2GFTVDQJq4Ul79L9dZEvPsjWh+k72Hs3lhy1H2qiGKeJDjvePrAubxYpniZ/8DDLBaWvMu/Q28EF7Q1OyytLdm61mFIN5FaCZm+f1sA3aevHyqgdFiyoXWf9mQEGnVw3XJn7UKbaJksKVFbZMAuKvpLSlcTw1Ja629rTRNLtHVV1L+e+l/Fwp5ar/Q68C0ir4KpBZU+m0RWKemguxeiWsNe/Q2zz14N0DOH5CsMZptxtX6mrte7pPh/XS1DW87jTFM6HYWXKwrurlYnT2+kWy32rix/e2MVl3QCEP6PJDhswZIGtdfA0tmXSzOqYG0r0NelbgjYPIt82UL/g1wYE/pV6nhLnGIQ+RBRQEJzNYlF+Qxods+lZkXTd+1BoKXcd6AU1SFkMHb4sr+qKErI3RfnfxFGY70VwfL33wyssFql8PFBbuqydPzZWGIaquo1DaqHKOe6lImlM+RRdiLFKsJUzSIGfYiJM4nc60EGhL8dnD78/Wk7WhqfJ63sjlLjEgg13tf1NDZNNAkKPYSQQKHfM6ltc9UW0Z0MMltGUaL0FTgqzOctVgoaL4/3JFTLw3F+IGT1cxgUN9jfxGcD0SbbLSVfUaojET0PSwfgSIKC7Z3cG6igjL0MiDuJjnoFvVdOz/9AplV81VAO9sUXA5FdT+L9CfmocCDrQ3ZG09dxsBrGyIfBlQJRsO+3MV/E0kMOMvUaiCBlNBq+7XeaHJEkOjKpIDyEDJGJOIkjjrCIkNSudtdPNtFmvuz4BnuWKgc4ut1T8Z+GtopLZBgXvwZVhrRaEYD112kBnjsxuykXenXhOAR7LjPvXB6T2LjlVlEgpjx/wc6PA3R9uKpW372t5/5wU3LcUNIaU8meo+P0zT78QGUMMSaWP8zSxf90DNSyLe3QOHKi2CKSVhMc8RN6zW6t00/ROXKR8SwiABEe5YevAd7gLt9xxb2fV8ecMYdsAbh09iVcsktTg7DQsT2NJZxEOz6JVmFkd+X01eOPvHdCRSExRNsD0xEgTVrY69qDHu9k6flv6m03ROoKXHSGlkbzksLqU4HJXTahV650Q7XE/u2V4yWHKDl5CYZ29L2mWyxUYux6a2qk9zHJbAf5rbOBlB7+hy6kRGVFhjc65x4Wr4xajwDU6R8EInEWSWkrmGgwfvTg3anexCNWd0MUUFQ+xDQ2yBv2KoKA3r++jdEUro/KoKdVjiG8Zs0w/SXeR2bqqrZxwsrexQQNnBfcZYLLyHomjm+ZufMkKkvaqlRfnMyau0qaKKxHZGT/qAm5zSNnpgpVhvWlyTNwy51xKv9jAW0sVo4e1sYlo2BWi3NDN1CW79xeZ6gXR/ATtkOFVpSzQucdqhuGQV6fJ2wB72MwJbyh+Iws3ULFn1fZwi75B7Mwz8CBcq7pSd57PjaYpwuyFb0xTcwQyNLhlhTtP6hhrXasHWIFSHz96BmLVsL+K79BjBEr7zHKF71oF+TwpuHQwspQgj1LCZcLAx8jZ9xrNvQT+d8YbB9mS8QKOcCyJW+BTfGCcSuowHKuwcdZhjoi30IBXHWSBePuM+QvbAB2t83XSDbJ+O/xuPc7dIYfD9JndizumkFNV2HIrv/bwd2QQmR3F2yH1kWxBBP9W08Qb8Rq59K51/Cir2eyeFIEAtzzCnrAR6/2BzGu0+/xeyYh0RR53CdF+WBsXFBw8QTQBHxcFMJ0+LNlIyO+pw+K27rtdxK+ac0Vg/CkuBwLLLVCiU5JqWsYJU+MtO5Nd6VI0Yl+mW0blE/kqSZTi7Fp18FxWE0Zq8Mop0cjArrmRL+vMeWhA34XW5f+iAoCq4r+kQcXG3sIlDPihVj5G6PQ7+GtRHC53KHUrFs/QPIoHqQQGEd/4z0PNFiUnxUFDteJsFfnRuaC4lvQB2vPgyuFEXpbAlF5g/J/yAGqzCpqXG9zPPNj3wU5ju44oIIGUUgG6IbxMtjNA+MqDfSihB+n8v7PsB/ggqI9J7wHuHDL63NlCKYNylVmUIspaoSJcy+HXmYRgcVCInhlAw+XM8ncJe9qQWny5sZfzsXLoTUB3D2r643I68medfzfPrgmHxM2q4IS/tzSbXQEd5Som2AmHXtHFzyRSCYa6vffbNzLUyOWwSZLXQ8I7ko5QjuHqcu+C8AluSUj+4im1AAdc88ZOiMD9vsrvezNj8a6btqog5c9UUbV/kwWrkBdnLcWhJilae9Ibkl/P7Johir/gfjloyu7xAoa81MRKNBdEe5b5fRv887ifamDwVqboX1Y6/wsGcgTiG8cGdxwN3wLrxS3uMJIxrKB8PEEsGNmRWhujOptZSQv/URdXqH1QvzqzLxLK9I8PQiP6OV2oKijQKzigs0NyDnqwlld4QXqiUdrXmDRc+sD1vN++/7HriG+1SYxPwX2IwpKh41jZBByDvGDK7TvhluhaItJ3CowIcoa6h/HDties+U1zpSP74YsevTecjI0D3+ebpyLf7M7O1N1yFtFNL7yEWG+/IB617Gw1ux9UlAxO/t2Pa8wYBaExBsEt+L4WzpNwWXUq62ezvMTmANxPO6xIgEhIBq+z39d0ccSk+A715Iz91o2hU5gGQ3UtkDymtEUSttb4fi6aXSEJtPBB+GSN0flBgSmOsjN1bbO5sJGUtryE8n6N/e5WS8mE7prVVt8NDfZ5vtu21WyTHDwq8LnsqNKOa3tWRuXQgne0Y5eXoIGdKGJJHRZ6oGSqXJ/vHMNuiMPlAOwJXjPg5tHAhahY+hRdKa+CibzoaqQUJrKqrnT+x/+08T77wtwx2f40mWcO7EkB3vqCNeqDblf7emdv4tx+PrrKxhrzcdG9pxnAMYIi8su67Iv+iGd12Dn3kRONS8YzZoOe4UfEeTQevhOwfJQDe6AOGd5/phypXteav6tyTeaZH3kdtdCrItdG/H2oQ6fwKuCTCt/m22t44K+dAbdovBXSJZtrGFTN8sr9uzvVMC9sugVUpM495HqIYRYsuYYtJNX2PvR+TRP+yLh+NTcsjD9lXHBxd8HWiVyiu4fHZvYUjBaxb8xZsUHnW+gK3xHotEVcBS9QdyeDxjF45NHktc3+YjavEtRS5v5MM4YQtk6kiW9W4vvSdeZKAG48+b3k6gEuJWc8TWsUjgrlqfSp1JnNR+ld8yu82C30fOJ54/V1mxNEqYYu3YQHArIqltYHU+C9Iy+qTnOCBxDAD5um5H4gDJcBrzdZU+1pMwlIT/uiTsBvNPfpatyuXgu6d+COkGVOIQxGG9oBZhAhg6gn63iqfHZYmmcFZxmvM+96txVlzOp+zyuF0SQ6vbGenbe4eM6CYoxFxVBXnylBag9wsmWXubEvEKIsPOwg7c9L6fmHQE36+v4ai/oSu+uZFSvmtBnjVkYnV8kZuslc0GuvCfcaSSTWpQLfejEgy+nOjVD6FQAY9K9iMhRMCapzt4wWPnIcBzIbUulGtXknoLMKj6SxZCs/TuG/oBS5a5sVfbXB6PrvEpYJsxBoWj5UkxbK5gMf0jR5ArjL8lyF2Uw2UpVDTmFZUnsPR3p8Wz+amla+Hc3fkd0kDqf66SWESibBkg96jqsMT0sM7hEqvou3bmdWL+SV+ljqQ8HHAtUUMcMritoeaHFM/KST+zQj6LeAOJWqbohU/wwsItnZ280DGQsQvt+/i9+2yCTxHTlXFiPWVyhlvpM2yvfWyV/P8N/mPiR+egENTPoL8Pjrzwx8oB+cwnSU+ISwebzDITuY9H1QrX+reTMGv8KY38y5AjDVg3p68OKAxKnHo4+dEohwalc6XIaDm6kclUpa5P3GvslalTVbE3LmlZt39iw69eOIeaBgiKfA5ybMontmKJ/aP6PtJdL9vyvR6l2B14VHPfol3w5fQ3PQ0ycsDWi535TtCoVUjyg3+ao45Fl+wUxolgvhcOWW4zcpLfJRLh73MwoeXRw8bdpDkM7BM904w3wg5+0PnjUVkzpcnTy1nR8sO9qGX18SJOeDQGwCjha2qNeC07Vcai3lrJ3JJVsCQqQwW1Dr9QmUWNhsi5am3VUOIspMdFS4VbGLjfkO5XVsNFmIaBMjPBzLCGML15Ecb2ykyMR0xeHxQhUFj5l19SEECdLCCAtEs0Avt1AyFpyXv++J5BfaNlWLf8G8huzo4v3MzlHsBCH4N5+at3bMkVVi6W59bQ+lbAl93uCs4KMCP0puqw5ex+q36mtIikdhGP8SOpCVE2YkOpdjZDI+x0uit1H6PJwZ0DmFGnf7CtNXM19qGg4540l8mJoPxZioFL1DdSrzZdDHGcu+cg1i5HR6/wt8rJcS4N7ySNaB6t+BUVsgSKswpndZ+QXK03DcX+vY85qUEZCV4VjamdntUW/w5r7NB1EeIyUdMkYlbXQKZC+PkLDsOGmbVa4fvIdZc7Nl8O41lydGR82ygGlxR5K3qLTd/fVLHm/UHFhbpBYTI7m+HniPUUt/arjxnzYz28FnKm0SEND3fgbA/aJZ9gQG6QBNfD5wnzuHlOjNcQQ5tZ8g/iNmNennicaA1g9OgXDAREZ5l6UDC/RyjGaVzkl/LFcKKmktOy/fg6PJLTgoqA08+kJG9VF7MyQWY0NYDkrTt+AR0ugz/H8ucAqNN1wv3Wfb8FleAQVRjZkx3lqSc10DUBvOJT1XEZcmf5sIx/UkRKHLNJELgBDSaobBTeQkVw1GP8vyEeVfWE9dXdXWB4+V0oTjHUYj13bWewsj8dOZ/fXaSRq6Xr5GB+tTVpe1/Vh/cb5t2MSJmAloJ8eLmIY5wFY2HY+vm1hcvkh0qu8DCDS81XS8GPDgfoQCZl2TIpGuvXKy5iqpxNE0iV/N3kbvrUfmsF3qtxiZtIQsLEXQDIC8P/8bAkX+3hCzA6t/NwXQ3z8ruD55awFZ7BCMT1LdQ2KWDyxboCGD1uxKJE2bPENHbvKIQDjRG3QxgC03tQKVPMOEq7PW2mf469UGghHqm6OIEcRQQM7u7/eNRWJaTa+fyBpKBRkWza34+261Oir3WwEWR/uzaLL/B+lOkzXtilyX0e4j24pINSZdDq0rPAuP8LgMb3WPNRb+2q3wjPrNSDuVV9lOo/viSVE2clpiXtaHGyRTbpfhNiZKqC+BTxiOwBRZIAlChZXgsq8MvTqqxt0CTrceHYI1lYGZ5BywGiKp9ySJXs7kdoSt25UodKetMWh7UB0mBBcqzuMhag6QmJgHJFRk3b/HHuBOJj+c1X5psG3XNHl2f9CAhXQfypBgYkUZ/fYcnaHbS+L1RJXvVdiCTXi1qsco1KYwGSvHnjo9JMaQv8o18itahNBNxti8YXh8klRA1DxDfPsZNmIhLD9TkCMxOACo1KAcF1YMbGYZMeGDiU2SwVpbt9254yJxJQq1u472zGgxzBQN9ju6Zi8dAGxzRS4BBsog89heNV3Lq8hg38A5KjNP8tghBvM+yDE3WO9s04R06Fi6fbSkNEsIih6Dspf8BtSXQHa8cfVNYO/+mWEGG/4LnSvsVFCFlgH1f5WSh90fGkdImxu8yqxFPvAKmkfnx8sZsZPiBl17ZOLKFFO4IMrT9UPyVrcJoLw2BNZcCYpRzS71zGFZP8H/cBYMjQho4yDRpOxC/iGB/byKMZBLF8zizBT/4l0D/xWQnvpI2eFPuCEYjLqjxUN+nKKIvpuDbMOvNj1csak9/UjkDgSHxxdqR3kWe7LAkCA/e12+loxjuE0kSOxVvfm1vEv/rDOPhHKMnUthReIYMYTQU2XDkp32jMDK8UTk0DferVPwyyjJL3trX1aIA7RzlG4YDMryJ5e260lgKdAKt9b1lZpQxqPSG4CF3HOftBRMcg6vSkC5xAIQ50t+//n2/0+9//zz/fmPV5aczemQRVA9vT/85u7FQ+2FuDg61AuG0w7n+TRyqAhyW8lVwJe'))
+PIECE_TYPES = ['pawn', 'king']  # Types de pièces
+NUM_PIECES = len(PIECE_TYPES) * 2  # Nombre de types de pièces (pour CREAM et BLACK)
+HASH_SIZE = 2 ** 20  # Taille de la table de transposition
+
+# Zobrist Hashing
+zobrist_table = {}
+for piece in ['black_pawn', 'black_king', 'cream_pawn', 'cream_king']:
+    for row in range(ROWS):
+        for col in range(COLS):
+            zobrist_table[(piece, row, col)] = random.getrandbits(64)
+
+# Transposition table
+transposition_table = {}
+
+def calculate_zobrist_hash(board):
+    hash_key = 0
+    for row in range(ROWS):
+        for col in range(COLS):
+            piece = board.get_piece(row, col)
+            if piece != 0:
+                piece_type = 'pawn' if piece.is_pawn() else 'king'  # Assurez-vous d'avoir une méthode pour vérifier le type de pièce
+                color = 'black' if piece.color == BLACK else 'cream'
+                piece_identifier = f"{color}_{piece_type}"
+                hash_key ^= zobrist_table[(piece_identifier, row, col)]
+    return hash_key
+
+
+def alphabeta_with_hash(position, depth, color_player, alpha, beta, game):
+    hash_key = calculate_zobrist_hash(position)
+
+    # Vérifiez si la position a déjà été évaluée
+    if hash_key in transposition_table:
+        print("is existe")
+        return transposition_table[hash_key] # Retournez l'évaluation précédemment stockée
+
+    if depth == 0 or position.winner(game.turn) != None:
+        return position.evaluate(color_player), position
+
+    best_move = None
+    for move in position.get_all_moves(position, color_player):
+        evaluation = -alphabeta_with_hash(move, depth-1, CREAM if color_player == BLACK else BLACK, -beta, -alpha, game)[0]
+        if evaluation > alpha:
+            best_move = move
+            alpha = evaluation
+            if alpha >= beta:
+                break  # Coupez l'arbre
+
+    # Stockez l'évaluation dans la table de transposition
+    transposition_table[hash_key] = (alpha, best_move)
+    '''
+    # Vous pourriez ici ajouter une logique pour gérer la taille de la table
+    if len(transposition_table) > HASH_SIZE:
+        # Éliminer une entrée, par exemple, la première ajoutée (pour simplifier)
+        transposition_table.pop(next(iter(transposition_table)))
+    '''
+    return alpha, best_move 
+
+
+
+# Alpha-béta avec ordonnancement *********************************************************
+
+history_table = {}  # Dictionnaire pour stocker les évaluations des mouvements
+
+def update_history(move, depth):
+    if move not in history_table:
+        history_table[move] = 0
+    # Plus le coup est profond dans l'arbre, plus il est important
+    history_table[move] += 2 ** depth  
+
+
+def createsCaptureOpportunity(board, piece):
+    """
+    Vérifie si après un mouvement, une capture est imminente.
+    """
+    # Chercher tous les mouvements valides pour cette pièce
+    valid_moves = board.get_all_moves(board, piece)
+    
+    # Si un des prochains mouvements permet une capture, cela crée une opportunité tactique
+    for move, skip in valid_moves.items():
+        if skip:  # Si un mouvement mène à une capture
+            return True
+    return False
+
+# Récupérer les coups de capture uniquement pour Quiescence Search
+def get_capture_moves(board, color):
+    capture_moves = {}
+    
+    for piece in board.get_all_pieces(color):
+        valid_moves = board.get_valid_moves(piece)
+        for move, skip in valid_moves.items():
+            if skip:  
+                temp_board = deepcopy(board)
+                temp_piece = temp_board.get_piece(piece.row, piece.col)
+                new_board = board.simulate_move(temp_piece, move, temp_board, skip)
+                capture_moves[new_board] = skip
+
+    return capture_moves
+
+def isTacticalMove(board, move, piece):
+    """
+    Vérifie si le coup est tactique.
+    Un coup est tactique s'il implique une capture, une promotion ou une menace directe.
+    """
+    if not piece.is_pawn():
+        return True
+
+    # Vérifier les menaces de captures directes après ce coup
+    if createsCaptureOpportunity(board, piece):
+        return True  # Menace de capture future
+
+    return False  # Sinon, ce n'est pas un coup tactique
+
+
+# Fonction de Quiescence Search
+def quiescenceSearch(board, alpha, beta, color_player, game):
+    # Évaluer la position actuelle
+    standPat = board.evaluate(color_player)
+
+    # Beta cutoff
+    if standPat >= beta:
+        return beta
+
+    # Mettre à jour alpha si l'évaluation est meilleure
+    if standPat > alpha:
+        alpha = standPat
+
+    # Rechercher les coups tactiques uniquement (captures ou menaces)
+    for piece in board.get_all_pieces(color_player):
+        valid_moves = board.get_valid_moves(piece)
+        for move, skip in valid_moves.items():
+            if isTacticalMove(board, move, piece):
+                # Simuler le coup
+                new_board = deepcopy(board)
+                new_board.move(piece, move[0], move[1])
+                if skip:
+                    new_board.remove(skip)
+                
+                # Recherche récursive
+                score = -quiescenceSearch(new_board, -beta, -alpha, CREAM if color_player == BLACK else BLACK, game)
+
+                # Beta cutoff
+                if score >= beta:
+                    return beta
+                if score > alpha:
+                    alpha = score
+
+    return alpha
+
+'''
+#*********** Negamax with killer moves , ordering moves and quiescence search optimizations  ***********#
+def NegaMax(position, depth, color_player, alpha, beta, game, killer_moves):
+
+    if depth == 0:
+        return quiescenceSearch(position, color_player, alpha, beta, game), position
+
+    if position.winner(game.turn) is not None:  # Si le jeu est terminé
+        return position.evaluate(color_player), position
+
+    best_move = None
+    moves = get_all_moves(position, color_player)
+
+    # Triage des mouvements avec Killer Moves
+    if depth in killer_moves:
+        killer_move = killer_moves[depth]
+        if killer_move in moves:
+            moves.remove(killer_move)
+            moves.insert(0, killer_move)  # Place le killer move en premier
+
+    for move in moves:
+        evaluation = -NegaMax(move, depth-1, CREAM if color_player == BLACK else BLACK, -beta, -alpha, game, killer_moves)[0]
+
+        if evaluation > alpha:
+            best_move = move
+            alpha = evaluation
+
+            if alpha >= beta:
+                update_history(best_move, depth)
+                break  # Coupure alpha-beta
+
+    # Enregistrer le meilleur mouvement dans la table des killer moves
+    if depth not in killer_moves:
+        killer_moves[depth] = best_move
+    else:
+        if best_move != killer_moves[depth]:
+            killer_moves[depth] = best_move
+
+    return alpha, best_move
+'''
+#*********** Negamax with killer moves and ordering moves optimizations  ***********#
+def NegaMax(position, depth, color_player, alpha, beta, game, killer_moves):
+
+    if depth == 0 or position.winner(game.turn) is not None:
+        return position.evaluate(color_player), position
+    
+    best_move = None
+    moves = position.get_all_moves(position, color_player)
+
+    # Triage des mouvements avec Killer Moves
+    if depth in killer_moves:
+        killer_move = killer_moves[depth]
+        if killer_move in moves:
+            moves.remove(killer_move)
+            moves.insert(0, killer_move)  # Place le killer move en premier
+
+    for move in moves:
+        evaluation = -NegaMax(move, depth-1, CREAM if color_player == BLACK else BLACK, -beta, -alpha, game, killer_moves)[0]
+        
+        if evaluation > alpha:
+            best_move = move
+            alpha = evaluation
+            
+            if alpha >= beta:
+                update_history(best_move, depth)  
+                break  # Coupez l'arbre
+
+    # Enregistrer le meilleur mouvement dans la table des killer moves
+    if depth not in killer_moves:
+        killer_moves[depth] = best_move
+    else:
+        if best_move != killer_moves[depth]:
+            killer_moves[depth] = best_move
+
+    return alpha, best_move
+
+
+
